dfac3ec1b940a472ccab180cdfca00c4b1585e25
Sabrina <ssamuelrome@gmail.com>
2022-04-21 17:02:15 +0100

    Added files from lessons 1-3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Wordle/i);
+  expect(titleElement).toBeInTheDocument();
 });

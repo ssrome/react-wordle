28b64850be891d8b141c2f514cb8400b41a02699
Sabrina <ssamuelrome@gmail.com>
2022-04-22 13:23:31 +0100

    Added code to track the guesses from lesson 4

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,14 +1,28 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const useWordle = (solution) => {
-  //   const [turn, setTurn] = useState(0);
-  //   const [currentGuess, setCurrentGuess] = useState("");
-  //   const [guesses, setGuesses] = useState([]); // each guess is an array
-  //   const [history, setHistory] = useState([]); // each guess is a string
-  //   const [isCorrect, setIsCorrect] = useState(false);
-  //   const formatGuess = () => {};
-  //   const addNewGuess = () => {};
-  //   const handleKeyup = () => {};
-  //   return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([]); // each guess is an array
+  const [history, setHistory] = useState([]); // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false);
+  const formatGuess = () => {};
+  const addNewGuess = () => {};
+  const handleKeyup = ({ key }) => {
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
+    }
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        });
+      }
+    }
+  };
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
 };
 export default useWordle;

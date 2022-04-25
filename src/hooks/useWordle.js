b2c0d3f92ddc082b2d26637dcfa807ddf15e43b7
Sabrina <ssamuelrome@gmail.com>
2022-04-25 17:11:00 +0100

    Added code from lesson 5 - submitting guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,11 +4,31 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
+  const [history, setHistory] = useState(["craft", "raise"]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("Formatting the guess - " + currentGuess);
+  };
   const addNewGuess = () => {};
   const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("You've used all your guesses");
+        return;
+      }
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("You've already tried that word");
+        return;
+      }
+      // check word is 5 characters long
+      if (currentGuess.length !== 5) {
+        console.log("Word must be 5 characters long");
+        return;
+      }
+      formatGuess();
+    }
     if (key === "Backspace") {
       setCurrentGuess((prev) => {
         return prev.slice(0, -1);

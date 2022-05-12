49ede81c9bc9c9f3d9e5e1da1788b6d7f17d0a97
Sabrina <ssamuelrome@gmail.com>
2022-05-12 12:29:37 +0100

    Added code for modal

---
+++ src/components/Model.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Model({ isCorrect, turn, solution }) {
+  return (
+    <div className="modal">
+      {isCorrect && (
+        <div>
+          <h1>You Win!</h1>
+          <p className="solution">{solution}</p>
+          <p>You found the solution in {turn} guesses 😊</p>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>Nevermind!</h1>
+          <p className="solution">{solution}</p>
+          <p>Better luck next time ☹️</p>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
+import Model from "./Model";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } =
     useWordle(solution);
+  const [showModel, setShowModel] = useState(false);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
     if (isCorrect) {
       console.log("Congrats, you win!");
+      setTimeout(() => setShowModel(true), 2000);
       window.removeEventListener("keyup", handleKeyup);
     }
 
     if (turn > 5) {
       console.log("Hard luck, you're out of guesses");
+      setTimeout(() => setShowModel(true), 2000);
       window.removeEventListener("keyup", handleKeyup);
     }
 
@@ -29,6 +33,9 @@ export default function Wordle({ solution }) {
       <div>current guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModel && (
+        <Model isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
     </>
   );
 }
